68ad3f4f59893d7d1435ae90477fb79bb834b27f
shamimarifin <shamimarifin790@gmail.com>
2025-07-25 19:43:42 +0600

    ful Comppleted Projectl

---
+++ scripts.js
@@ -0,0 +1,11 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.flower-item', { delay: 500 });
+ScrollReveal().reveal('.left-image', { delay: 500 });
+ScrollReveal().reveal('.right-content', { delay: 500 });
+ScrollReveal().reveal('.item', { delay: 1000 });
+
+
+const menuBtn = document.getElementById('#toggle i')
+const menuCancel = document.getElementsByClassName('.menu-cancel');
+const navLink   = document.getElementsByClassName('.nav_list')
+
